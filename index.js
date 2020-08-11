23c9bd58c2abe11c45245e63a18dca84750e1d66
fridmaster <fridmaster@gmail.com>
2020-08-11 22:10:40 +0300

    little fix

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = 3000;
 const bodyParser = require('body-parser');
 
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
 app.get('/', (req, res, next) => {
