fe2d462924d9e856fac1a10569d14dd07d79e5f1
fridmaster <fridmaster@gmail.com>
2020-08-11 22:00:11 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res, next) => {
+    res.send('Hello World!')
+    next();
+})
+
+app.get('/save-user', (req, res, next) => {
+    console.log('here we has request-body', req.body);
+    res.sendStatus(200);
+    next();
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
